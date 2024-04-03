e12fe7e30de8db4245d7a7822b13bbd782ecde76
csanders-insight <courtney.sanders@insight.com>
2024-04-03 13:19:29 -0400

    testing pool update

--- provider.tf
+++ provider.tf
@@ -10,3 +10,10 @@ terraform {
 provider "azurerm" {
   features {}
 }
+
+data "azurerm_subscription" "current" {
+}
+
+output "current_subscription_display_name" {
+  value = data.azurerm_subscription.current.display_name
+}
