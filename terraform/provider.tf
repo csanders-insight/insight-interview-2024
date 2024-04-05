bd468298e2bc3edb3d3be487b53ee4d10e7076dc
csanders-insight <courtney.sanders@insight.com>
2024-04-05 13:41:59 -0400

    storage accounts

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -18,27 +18,12 @@ output "current_subscription_display_name" {
   value = data.azurerm_subscription.current.display_name
 }
 
-resource "random_uuid" "random_uuid" {}
+resource "random_id" "random_id" {
+  byte_length = 2
+}
 
 resource "azurerm_resource_group" "myresourcegroup" {
-  name     = random_uuid.random_uuid.result
+  name     = random_id.random_id.dec
   location = "East US"
 }
-
-resource "azurerm_virtual_network" "mynetwork" {
-  name                = random_uuid.random_uuid.result
-  resource_group_name = azurerm_resource_group.myresourcegroup.name
-  location            = azurerm_resource_group.myresourcegroup.location
-  address_space       = ["10.0.0.0/16"]
-}
-
-# Subdivision of network
-# TODO: If we don't need this for other resources, move to vm.tf
-resource "azurerm_subnet" "mysubnet" {
-  name                 = random_uuid.random_uuid.result
-  resource_group_name  = azurerm_resource_group.myresourcegroup.name
-  virtual_network_name = azurerm_virtual_network.mynetwork.name
-  address_prefixes     = ["10.0.1.0/24"]
-}
-
 
--- terraform/virtual-machines.tf
+++ terraform/virtual-machines.tf
@@ -1,8 +1,24 @@
+
+resource "azurerm_virtual_network" "mynetwork" {
+  name                = random_id.random_id.dec
+  resource_group_name = azurerm_resource_group.myresourcegroup.name
+  location            = azurerm_resource_group.myresourcegroup.location
+  address_space       = ["10.0.0.0/16"]
+}
+
+# "namespace"
+resource "azurerm_subnet" "mysubnet" {
+  name                 = random_id.random_id.dec
+  resource_group_name  = azurerm_resource_group.myresourcegroup.name
+  virtual_network_name = azurerm_virtual_network.mynetwork.name
+  address_prefixes     = ["10.0.1.0/24"]
+}
+
 # Allow VMs to use the subnet
 resource "azurerm_network_interface" "mynic" {
   count = var.num_instances
 
-  name                = "${count.index}-${random_uuid.random_uuid.result}"
+  name                = "${count.index}-${random_id.random_id.dec}"
   resource_group_name = azurerm_resource_group.myresourcegroup.name
   location            = azurerm_resource_group.myresourcegroup.location
   ip_configuration {
@@ -16,7 +32,7 @@ resource "azurerm_network_interface" "mynic" {
 resource "azurerm_linux_virtual_machine" "myvm" {
   count = var.num_instances
 
-  name                            = "${count.index}-${random_uuid.random_uuid.result}"
+  name                            = "${count.index}-${random_id.random_id.dec}"
   resource_group_name             = azurerm_resource_group.myresourcegroup.name
   location                        = azurerm_resource_group.myresourcegroup.location
   size                            = "Standard_B1s"
